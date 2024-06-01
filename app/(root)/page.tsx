981a76ed73ba987cca6f9f14e32e9d5ee3b67409
Aymen Gabsi <aymengabsi20@gmail.com>
2024-06-01 17:52:00 +0200

    [Podcastr] Configure Tailwind & set The File Structure.

---
+++ app/(root)/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+import { Section } from "lucide-react";
+
+const Home = () => {
+  return (
+    <div className="mt-9 flex flex-col gap-9">
+      <Section className="flex flex-col gap-5">
+        <h1 className="text-20 font-bold text-white-1">Trending Podcasts</h1>
+        <Button className="text-white-1 bg-orange-1">Button</Button>
+      </Section>
+    </div>
+  );
+};
+
+export default Home;
